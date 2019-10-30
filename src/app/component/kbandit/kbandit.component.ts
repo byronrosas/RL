5b221cb3d810eba6e3ed8473d6a76dbb3de641c4
Byron Rosas <byron_mma@hotmail.es>
2019-10-30 00:57:14 -0500

    algo UCB bandit

--- src/app/component/kbandit/kbandit.component.ts
+++ src/app/component/kbandit/kbandit.component.ts
@@ -16,12 +16,16 @@ export class KBanditComponent implements OnInit {
   arrBtnTest:any[];
   arrBtnTest_E_GREEDY_MEAN:any[];
   arrBtnTest_E_GREEDY_INCREMENTAL:any[];
+  arrBtnTest_UCB:any[];
+  arrFail:any=[0,0,0,0,0];
+  arrWin:any=[0,0,0,0,0];
   objGraphicData:any=0;
 lb;
 data;
 DATA_USER=[];
 DATA_E_GREEDY_MEAN=[];
 DATA_E_GREEDY_INC=[];
+DATA_UCB=[];
   constructor() { 
     this.medicinas=0;
     this.pacientes=0;
@@ -50,9 +54,10 @@ DATA_E_GREEDY_INC=[];
     this.arrBtnTest=new Array();
     this.arrBtnTest_E_GREEDY_MEAN=new Array();
     this.arrBtnTest_E_GREEDY_INCREMENTAL=new Array();
-
+    this.arrBtnTest_UCB=new Array();    
     this.EGreedyMean(this.pacientes,this.medicinas,0.2);
     this.EGreedyIncremental(this.pacientes,this.medicinas,0.2);
+    this.UCB(this.pacientes,this.medicinas);
   }
 
 
@@ -167,13 +172,44 @@ DATA_E_GREEDY_INC=[];
 
   UCB(T,k)
   {
+    let t=0;
     
+    let q=[];
+    q.length=k;
+    q.fill(0);
+
+    // n: número de veces que una acción ha sido tomada
+    let n=[...q];
+    let a =new Array(k);
+    let UCB=new Array(k);
+    for (let index = 0; index < k; index++) {      
+      q[index]=this.tomarMedicina_UCB(index).result ? 1:0; 
+      n[index]++;
+      t++;
+    }    
+    while(t<T)
+    {
+      for (let index = 0; index < k; index++) {      
+        a[index]=Math.sqrt((2*(Math.log(T)))/n[index]); 
+        UCB[index]=q[index]+a[index];
+      }
+      let accion_max=UCB.indexOf(Math.max(...UCB));
+      let r_obtenido=this.tomarMedicina_UCB(accion_max).result ? 1:0;
+        // aumentamos en uno el número de veces que se ha tomado esta acción
+        n[accion_max]++;
+        // recalculamos el valor de la accion
+        q[accion_max]=(q[accion_max]+r_obtenido)/n[accion_max];
+      t++;
+    }
   }
 
   
 
   clickMedicina(i)
-  {       
+  { 
+    this.arrFail[0]=0;
+    this.arrWin[0]=0;
+
     let r = Math.floor(Math.random() * 101) < this.arrBtnMedicinas[i] ? true : false;    
     let T=[...this.arrBtnMedicinas];             
     T.fill(0);
@@ -185,18 +221,24 @@ DATA_E_GREEDY_INC=[];
     this.arrBtnTest.forEach((obj)=>{            
       if(obj.result)
       {
-        T[obj.medicina]=T[obj.medicina]+1;
-      }      
+        T[obj.medicina]=T[obj.medicina]+1;        
+        this.arrWin[0]++;
+      }else{
+        this.arrFail[0]++;
+      }
     });      
     T.forEach((t)=>{
       this.DATA_USER.push(t/this.arrBtnTest.length);
     });
 
-    this.graficar(this.DATA_USER,this.DATA_E_GREEDY_MEAN,this.DATA_E_GREEDY_INC);
+    this.graficar(this.DATA_USER,this.DATA_E_GREEDY_MEAN,this.DATA_E_GREEDY_INC,this.DATA_UCB);
   }
 
   tomarMedicina_egreedy_mean(i)
   {
+    this.arrFail[1]=0;
+    this.arrWin[1]=0;
+
     let r = Math.floor(Math.random() * 101) < this.arrBtnMedicinas[i] ? true : false;    
     let T=[...this.arrBtnMedicinas];             
     T.fill(0);
@@ -210,19 +252,25 @@ DATA_E_GREEDY_INC=[];
       if(obj.result)
       {
         T[obj.medicina]=T[obj.medicina]+1;
-      }      
+        this.arrWin[1]++;
+      }else{
+        this.arrFail[1]++;
+      }  
     });      
     T.forEach((t)=>{
       this.DATA_E_GREEDY_MEAN.push(t/this.arrBtnTest_E_GREEDY_MEAN.length);
     });
 
-    this.graficar(this.DATA_USER,this.DATA_E_GREEDY_MEAN,this.DATA_E_GREEDY_INC);
+    this.graficar(this.DATA_USER,this.DATA_E_GREEDY_MEAN,this.DATA_E_GREEDY_INC,this.DATA_UCB);
     return o;
   }
 
 
   tomarMedicina_egreedy_inc(i)
   {
+    this.arrFail[2]=0;
+    this.arrWin[2]=0;
+
     let r = Math.floor(Math.random() * 101) < this.arrBtnMedicinas[i] ? true : false;    
     let T=[...this.arrBtnMedicinas];             
     T.fill(0);
@@ -236,22 +284,60 @@ DATA_E_GREEDY_INC=[];
       if(obj.result)
       {
         T[obj.medicina]=T[obj.medicina]+1;
+        this.arrWin[2]++;
+      }else{
+        this.arrFail[2]++;
       }      
     });      
     T.forEach((t)=>{
       this.DATA_E_GREEDY_INC.push(t/this.arrBtnTest_E_GREEDY_INCREMENTAL.length);
     });
 
-    this.graficar(this.DATA_USER,this.DATA_E_GREEDY_MEAN,this.DATA_E_GREEDY_INC);
+    this.graficar(this.DATA_USER,this.DATA_E_GREEDY_MEAN,this.DATA_E_GREEDY_INC,this.DATA_UCB);
     return o;
   }
 
-  graficar(DATA_USER,DATA_E_GREEDY_MEAN,DATA_E_GREEDY_INC)
+
+  tomarMedicina_UCB(i)
+  {
+    this.arrFail[3]=0;
+    this.arrWin[3]=0;
+
+
+    let r = Math.floor(Math.random() * 101) < this.arrBtnMedicinas[i] ? true : false;    
+    let T=[...this.arrBtnMedicinas];             
+    T.fill(0);
+    this.DATA_UCB=[];
+    var o ={
+      medicina:i,
+      result: r
+    }
+    this.arrBtnTest_UCB.push(o);
+    this.arrBtnTest_UCB.forEach((obj)=>{            
+      if(obj.result)
+      {
+        T[obj.medicina]=T[obj.medicina]+1;
+        this.arrWin[3]++;
+      }else{
+        this.arrFail[3]++;
+      }      
+    });      
+    T.forEach((t)=>{
+      this.DATA_UCB.push(t/this.arrBtnTest_UCB.length);
+    });
+
+    this.graficar(this.DATA_USER,this.DATA_E_GREEDY_MEAN,this.DATA_E_GREEDY_INC,this.DATA_UCB);
+    return o;
+  }
+
+
+  graficar(DATA_USER,DATA_E_GREEDY_MEAN,DATA_E_GREEDY_INC,DATA_UCB)
   {
     
     this.data= [{ data:DATA_USER, label: 'Usuario' },
                 { data:DATA_E_GREEDY_MEAN, label: 'E_GREEDY_MEAN' },
-                { data:DATA_E_GREEDY_INC, label: 'E_GREEDY_INC' }];                 
+                { data:DATA_E_GREEDY_INC, label: 'E_GREEDY_INC'},
+                { data:DATA_UCB,label:'UCB'}];                 
     this.cambiarGrafico();              
   }
 
